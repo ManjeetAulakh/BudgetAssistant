18a76eb80d03ab862857a7f68702bc982f49ac25
Manjeet Singh <118804835+ManjeetAulakh@users.noreply.github.com>
2025-11-19 12:36:31 +0530

    first commit

---
+++ personalized-budget-assistant/src/main/java/com/budgetassistant/PersonalizedBudgetAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.budgetassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedBudgetAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedBudgetAssistantApplication.class, args);
+	}
+
+}
---
+++ personalized-budget-assistant/src/test/java/com/budgetassistant/PersonalizedBudgetAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedBudgetAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
